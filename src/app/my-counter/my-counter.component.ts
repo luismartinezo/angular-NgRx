930aa25e4e17561aca072d23527b4087e7eab203
luismartinezo <lmartinezocoro@gmail.com>
2021-06-17 16:35:09 -0500

    metodos compoenente counter y diseno

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { decrement, increment, reset } from '../counter.actions';
 
 @Component({
   selector: 'app-my-counter',
@@ -7,26 +9,27 @@ import { Observable } from 'rxjs';
   styleUrls: ['./my-counter.component.css']
 })
 export class MyCounterComponent implements OnInit {
+  count$: Observable<number>;
 
-  count$: Observable<number>
- 
-  constructor() {
+  constructor(private store: Store<{ count: number }>) {
     // TODO: Connect `this.count$` stream to the current store `count` state
+    this.count$ = store.select('count');
   }
- 
+
   increment() {
     // TODO: Dispatch an increment action
+    this.store.dispatch(increment());
   }
- 
+
   decrement() {
     // TODO: Dispatch a decrement action
+    this.store.dispatch(decrement());
   }
- 
+
   reset() {
     // TODO: Dispatch a reset action
+    this.store.dispatch(reset());
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
