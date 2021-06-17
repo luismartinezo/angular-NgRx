f4294efd12d588dbfd5b9ca58094db5784acccac
luismartinezo <lmartinezocoro@gmail.com>
2021-06-17 16:05:56 -0500

    Creacion del componente counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter.reducer';
 
 import { AppComponent } from './app.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 @NgModule({
   imports: [BrowserModule,StoreModule.forRoot({ count: counterReducer })],
-  declarations: [AppComponent],
+  declarations: [AppComponent, MyCounterComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
